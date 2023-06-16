ba0cc9d4e8ae7a42ff0f5e8327c187d20333e37f
Chris Raible <chris@ghost.org>
2023-06-16 13:42:56 -0700

    WIP - converting to use posts.json instead of mysql database

--- ghost/mobiledoc-lexical-migration-test/lib/mobiledoc-lexical-migration-test.js
+++ ghost/mobiledoc-lexical-migration-test/lib/mobiledoc-lexical-migration-test.js
@@ -2,51 +2,66 @@
 const mobiledocLib = require('../../core/core/server/lib/mobiledoc');
 const lexicalLib = require('../../core/core/server/lib/lexical');
 const mobiledocToLexical = require('@tryghost/kg-converters').mobiledocToLexical;
-const mysql = require('mysql');
 const {assertHTML} = require('./utils');
+const fs = require('fs');
+const path = require('path');
+const posts = require('./data/posts.json');
 
-const database = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'password',
-    database: 'database-name'
-});
+console.log(typeof posts);
 
-database.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
+// Clear out past results and recreate the results directory
+const resultsPath = path.join(__dirname, 'results');
+if (fs.existsSync(resultsPath)) {
+    fs.rmSync(resultsPath, {recursive: true});
+}
+if (!fs.existsSync(resultsPath)) {
+    fs.mkdirSync(resultsPath, {recursive: true});
+}
 
-    console.log('connected as id ' + database.threadId);
-});
-
-database.query('SELECT mobiledoc FROM posts;', function (error, results, fields) {
-    if (error) {
-        throw error;
-    }
-
-    for (let i = 0; i < results.length; i++) {
-        const mobiledoc = results[i].mobiledoc;
+const testPosts = async function () {
+    let failures = 0;
+    for (const post in posts) {
+        console.log(post);
+        console.log('Comparing post ' + post.uuid);
+        const mobiledoc = post.mobiledoc;
         const mobiledocHtml = renderMobiledocToHtml(mobiledoc);
         const lexicalHtml = renderConvertedMobiledocToHtml(mobiledoc);
-        assertHTML(mobiledocHtml, lexicalHtml);
+        if (lexicalHtml !== undefined) {
+            const result = await assertHTML(post.uuid, mobiledocHtml, lexicalHtml);
+            if (!result) {
+                failures += 1;
+            }
+        }
     }
-});
+    console.log('Total Posts: ' + results.length);
+    console.log('Failures: ' + failures);
+    console.log('Success Rate: ' + (results.length - failures) / results.length);
+};
+
+testPosts();
 
 const renderMobiledocToHtml = function (mobiledoc) {
     return mobiledocLib.mobiledocHtmlRenderer.render(JSON.parse(mobiledoc));
 };
 
 const renderConvertedMobiledocToHtml = function (mobiledoc) {
-    const lexical = mobiledocToLexical(mobiledoc);
-    return lexicalLib.render(lexical);
+    try {
+        const lexical = mobiledocToLexical(mobiledoc);
+        if (lexical) {
+            return lexicalLib.render(lexical);
+        }
+        return undefined;
+    } catch (err) {
+        console.log('🚨 Error converting mobiledoc to lexical: ', err);
+    }
 };
 
 const compareRenderedHtml = function (mobiledoc) {
     const mobiledocHtml = renderMobiledocToHtml(mobiledoc);
     const lexicalHtml = renderConvertedMobiledocToHtml(mobiledoc);
-    return assertHTML(mobiledocHtml, lexicalHtml);
+    if (lexicalHtml) {
+        return assertHTML(mobiledocHtml, lexicalHtml);
+    }
 };
 
 module.exports = {
--- ghost/mobiledoc-lexical-migration-test/lib/utils.js
+++ ghost/mobiledoc-lexical-migration-test/lib/utils.js
@@ -1,11 +1,14 @@
 const jsdom = require('jsdom');
 const prettier = require('prettier');
-const {expect} = require('@playwright/test');
 const mysql = require('mysql');
+const fs = require('fs');
+const path = require('path');
+let beautify = require('js-beautify').html;
 
 const {JSDOM} = jsdom;
 
 const assertHTML = async function (
+    postId,
     mobiledocHtml,
     lexicalHtml,
     {
@@ -20,7 +23,7 @@ const assertHTML = async function (
         ignoreCardCaptionContents = false
     } = {}
 ) {
-    const actual = prettifyHTML(mobiledocHtml.replace(/\n/gm, ''), {
+    const mobiledoc = await prettifyHTML(mobiledocHtml.replace(/\n/gm, ''), {
         ignoreClasses,
         ignoreInlineStyles,
         ignoreInnerSVG,
@@ -31,7 +34,7 @@ const assertHTML = async function (
         ignoreDataTestId,
         ignoreCardCaptionContents
     });
-    const expected = prettifyHTML(lexicalHtml.replace(/\n/gm, ''), {
+    const lexical = await prettifyHTML(lexicalHtml.replace(/\n/gm, ''), {
         ignoreClasses,
         ignoreInlineStyles,
         ignoreInnerSVG,
@@ -42,7 +45,18 @@ const assertHTML = async function (
         ignoreDataTestId,
         ignoreCardCaptionContents
     });
-    expect(actual).toEqual(expected);
+    if (mobiledoc === lexical) {
+        console.log('Post ID: ' + postId + ': ✅');
+        return true;
+    } else {
+        console.log('Post ID: ' + postId + ': ❌');
+        fs.mkdirSync(path.join(__dirname, 'results', postId), {recursive: true});
+        const mobiledocPath = path.join(__dirname, 'results', postId, 'mobiledoc.html');
+        const lexicalPath = path.join(__dirname, 'results', postId, 'lexical.html');
+        fs.writeFileSync(mobiledocPath, mobiledoc, {flag: 'w+'});
+        fs.writeFileSync(lexicalPath, lexical, {flag: 'w+'});
+        return false;
+    }
 };
 
 const prettifyHTML = async function (string, options = {}) {
@@ -74,6 +88,35 @@ const prettifyHTML = async function (string, options = {}) {
     // replace all instances of blob:http with "blob:..."
     output = output.replace(/blob:http[^"]*/g, 'blob:...');
 
+    // replace opening and closing tags with self closing tags
+    output = output.replace(/><\/path>/g, '/>');
+    output = output.replace(/><\/rect>/g, '/>');
+    output = output.replace(/><\/polyline>/g, '/>');
+    output = output.replace(/><\/line>/g, '/>');
+    output = output.replace(/><\/circle>/g, '/>');
+
+    // replace duplicate <em> tags with single <em> tag
+    output = output.replace(/<em><em>/g, '<em>');
+    output = output.replace(/<\/em><\/em>/g, '</em>');
+
+    // replace duplicate <string> tags with single <strong> tag
+    output = output.replace(/<strong><strong>/g, '<strong>');
+    output = output.replace(/<\/strong><\/strong>/g, '</strong>');
+
+    // replace empty attribute values with booleans
+    output = output.replace(/=""/g, '');
+
+    // replace b with strong
+    output = output.replace(/<b>/g, '<strong>');
+    output = output.replace(/<\/b>/g, '</strong>');
+
+    // replace i with em
+    output = output.replace(/<i>/g, '<em>');
+    output = output.replace(/<\/i>/g, '</em>');
+
+    // remove spaces between brackets
+    output = output.replace(/>\s+</g, '><');
+
     if (options.ignoreCardContents || options.ignoreCardToolbarContents || options.ignoreCardCaptionContents) {
         const {document} = (new JSDOM(output)).window;
 
@@ -94,15 +137,18 @@ const prettifyHTML = async function (string, options = {}) {
         output = document.body.innerHTML;
     }
 
-    return prettier
-        .format(output, {
-            attributeGroups: ['$DEFAULT', '^data-'],
-            attributeSort: 'ASC',
-            bracketSameLine: true,
-            htmlWhitespaceSensitivity: 'ignore',
-            parser: 'html'
-        })
-        .trim();
+    // const prettierFormatted = prettier
+    //     .format(output, {
+    //         attributeGroups: ['$DEFAULT', '^data-'],
+    //         attributeSort: 'ASC',
+    //         bracketSameLine: true,
+    //         htmlWhitespaceSensitivity: 'strict',
+    //         singleAttributePerLine: true,
+    //         parser: 'html'
+    //     })
+    //     .trim();
+
+    return beautify(output);
 };
 
 const getPosts = async function () {
