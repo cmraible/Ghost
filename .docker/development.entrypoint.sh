feabe0356488139157ae516d754c83b5fc49449c
Chris Raible <chris@ghost.org>
2025-02-19 15:33:22 -0800

    Added git submodule update to entrypoint with error handling

--- .docker/development.entrypoint.sh
+++ .docker/development.entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+if ! git submodule update --init --recursive; then
+    echo "Error: Failed to update git submodules"
+    echo "Please run the following command locally to update the submodules:"
+    echo "git submodule update --init --recursive"
+    exit 1
+fi
+
 # Execute the CMD
 exec "$@"
